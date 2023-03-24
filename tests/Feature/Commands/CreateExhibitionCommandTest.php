d383ec7528cb8384a4a50e54db26ba7717fd99cb
batista <ekyidag@gmail.com>
2023-03-24 18:33:39 -0300

    test: add tests to create exhibition command

--- tests/Feature/Commands/CreateExhibitionCommandTest.php
+++ tests/Feature/Commands/CreateExhibitionCommandTest.php
@@ -32,6 +32,136 @@ class CreateExhibitionCommandTest extends TestCase
         $this->seed(BasicRequiredDataSeeder::class);
     }
 
+//    /**
+//     * @dataProvider commandDataProvider
+//     */
+//    public function testCreateExhibitionCommand($appEnv, $expectEnvWarning, $filmData, $theaterData, $roomData, $startsAt, $dayOfWeek)
+//    {
+//        Bus::fake();
+//
+//        Config::set('app.env', $appEnv);
+//
+//        $film = Film::factory()->create($filmData);
+//        $theater = Theater::factory()->create($theaterData);
+//        $room = TheaterRoom::factory()->create(array_merge($roomData, ['theater_id' => $theater->uuid]));
+//
+//        $seatType = SeatType::firstWhere(['name' => SeatType::REGULAR]);
+//
+//        $rowCount = rand(2, 3);
+//        $seatCount = rand(3, 4);
+//
+//        for ($i = 0; $i < $rowCount; $i++) {
+//            $row = TheaterRoomRow::factory()->create([
+//                'name' => Str::orderedUuid()->toString(),
+//                'theater_room_id' => $room->uuid
+//            ]);
+//
+//            for ($j = 0; $j < $seatCount; $j++) {
+//
+//                TheaterRoomSeat::factory()->create([
+//                    'name' => Str::orderedUuid()->toString(),
+//                    'theater_room_row_id' => $row->uuid,
+//                    'seat_type_id' => $seatType->uuid
+//                ]);
+//            }
+//        }
+//
+//        $ticketTypes = TicketType::all();
+//
+//        $command = $this->artisan('create:exhibition');
+//
+//        if ($expectEnvWarning) {
+//            $command->expectsQuestion('This command is not suitable for production. Are you sure you want to continue?', 'yes');
+//        }
+//
+//        $command->expectsQuestion('Whats the film?', $film->name)
+//            ->expectsQuestion('Whats the theater?', $theater->name)
+//            ->expectsQuestion('Whats the theater room?', $room->name)
+//            ->expectsQuestion('Whats the time of the exhibition?', $startsAt)
+//            ->expectsQuestion('Which day of the week is it?', $dayOfWeek)
+//            ->expectsQuestion('What ticket types do you wanna add to the exhibition?', [$ticketTypes->first()->name])
+//            ->expectsQuestion("Whats the price for the {$ticketTypes->first()->name} ticket? (in cents)", $ticketTypes->first()->price);
+//
+//        $exitCode = $command->run();
+//        $this->assertEquals(0, $exitCode);
+//
+//        Bus::assertDispatchedTimes(CreateExhibitionSeatAvailabilityJob::class, 1);
+//        Bus::assertDispatchedTimes(PopulateExhibitionTicketPricingJob::class, 1);
+//    }
+//
+//    public static function commandDataProvider()
+//    {
+//        return [
+//            [
+//                'local',
+//                false,
+//                ['name' => 'Film 1'],
+//                ['name' => 'Theater 1'],
+//                ['name' => 'Room 1'],
+//                '14:00',
+//                'Monday',
+//                [
+//                    ['name' => 'Regular', 'price' => 1000],
+//                    ['name' => 'Student', 'price' => 800],
+//                    ['name' => 'Courtesy', 'price' => 0],
+//                ],
+//            ],
+//            [
+//                'production',
+//                true,
+//                ['name' => 'Film 2'],
+//                ['name' => 'Theater 2'],
+//                ['name' => 'Room 2'],
+//                '16:30',
+//                'Wednesday',
+//                [
+//                    ['name' => 'Regular', 'price' => 1200],
+//                    ['name' => 'Student', 'price' => 900],
+//                    ['name' => 'Courtesy', 'price' => 0],
+//                ],
+//            ],
+//            [
+//                'local',
+//                false,
+//                ['name' => 'Film 3'],
+//                ['name' => 'Theater 3'],
+//                ['name' => 'Room 3'],
+//                '19:45',
+//                'Friday',
+//                [
+//                    ['name' => 'Regular', 'price' => 1300],
+//                    ['name' => 'Student', 'price' => 1000],
+//                    ['name' => 'Courtesy', 'price' => 0],
+//                ],
+//            ],
+//        ];
+//    }
+//
+//    /**
+//     * @dataProvider validTimeDataProvider
+//     */
+//    public function testValidTimeValidation($validTime)
+//    {
+//        $this->testCreateExhibitionCommand(
+//            'local',
+//            false,
+//            ['name' => 'Film 1'],
+//            ['name' => 'Theater 1'],
+//            ['name' => 'Room 1'],
+//            $validTime,
+//            'Monday'
+//        );
+//    }
+//
+//    public static function validTimeDataProvider()
+//    {
+//        return [
+//            ['00:00'],
+//            ['14:30'],
+//            ['23:59'],
+//        ];
+//    }
+
     /**
      * @dataProvider commandDataProvider
      */
@@ -100,11 +230,6 @@ class CreateExhibitionCommandTest extends TestCase
                 ['name' => 'Room 1'],
                 '14:00',
                 'Monday',
-                [
-                    ['name' => 'Regular', 'price' => 1000],
-                    ['name' => 'Student', 'price' => 800],
-                    ['name' => 'Courtesy', 'price' => 0],
-                ],
             ],
             [
                 'production',
@@ -114,11 +239,6 @@ class CreateExhibitionCommandTest extends TestCase
                 ['name' => 'Room 2'],
                 '16:30',
                 'Wednesday',
-                [
-                    ['name' => 'Regular', 'price' => 1200],
-                    ['name' => 'Student', 'price' => 900],
-                    ['name' => 'Courtesy', 'price' => 0],
-                ],
             ],
             [
                 'local',
@@ -128,30 +248,34 @@ class CreateExhibitionCommandTest extends TestCase
                 ['name' => 'Room 3'],
                 '19:45',
                 'Friday',
-                [
-                    ['name' => 'Regular', 'price' => 1300],
-                    ['name' => 'Student', 'price' => 1000],
-                    ['name' => 'Courtesy', 'price' => 0],
-                ],
             ],
-        ];
-    }
-
-    /**
-     * @dataProvider validTimeDataProvider
-     */
-    public function testValidTimeValidation($validTime)
-    {
-        // You can reuse the existing testCreateExhibitionCommand test method with the valid time
-        $this->testCreateExhibitionCommand('local', false, ['name' => 'Film 1'], ['name' => 'Theater 1'], ['name' => 'Room 1'], $validTime, 'Monday');
-    }
-
-    public static function validTimeDataProvider()
-    {
-        return [
-            ['00:00'],
-            ['14:30'],
-            ['23:59'],
+            [
+                'production',
+                true,
+                ['name' => 'Film 4'],
+                ['name' => 'Theater 4'],
+                ['name' => 'Room 4'],
+                '00:00',
+                'Monday',
+            ],
+            [
+                'local',
+                false,
+                ['name' => 'Film 5'],
+                ['name' => 'Theater 5'],
+                ['name' => 'Room 5'],
+                '14:30',
+                'Tuesday',
+            ],
+            [
+                'production',
+                true,
+                ['name' => 'Film 6'],
+                ['name' => 'Theater 6'],
+                ['name' => 'Room 6'],
+                '23:59',
+                'Sunday',
+            ],
         ];
     }
 
