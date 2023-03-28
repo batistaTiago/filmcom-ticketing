6e7a43eb0c0f77f4d8f39eb349709e4b89ce3798
batista <ekyidag@gmail.com>
2023-03-28 12:53:57 -0300

    refactor/checkout

--- app/Repositories/MysqlCartRepository.php
+++ app/Repositories/MysqlCartRepository.php
@@ -4,8 +4,10 @@ namespace App\Repositories;
 
 use App\Domain\DTO\Cart\CartDTO;
 use App\Domain\DTO\Cart\CartStatusDTO;
+use App\Domain\DTO\UserDTO;
 use App\Domain\Repositories\CartRepositoryInterface;
 use App\Domain\Repositories\CartStatusRepositoryInterface;
+use App\Exceptions\ResourceNotFoundException;
 use App\Models\Cart;
 use App\Models\CartStatus;
 use Illuminate\Support\Str;
@@ -17,14 +19,30 @@ class MysqlCartRepository implements CartRepositoryInterface
         return Cart::query()->firstWhere(compact('uuid'))->toDto();
     }
 
+    public function getActiveUserCart(string $uuid, UserDTO|string $userInput): CartDTO
+    {
+        $user_id = $userInput instanceof UserDTO ? $userInput->uuid : $userInput;
+
+        $cart = Cart::query()
+            ->where('uuid', $uuid)
+            ->whereHas('user', fn ($query) => $query->where('uuid', $user_id))
+            ->whereHas('status', fn ($query) => $query->where('name', CartStatus::ACTIVE))
+            ->first();
+
+        if (empty($cart)) {
+            throw new ResourceNotFoundException('No active cart was found for this user');
+        }
+
+        return $cart->toDto();
+    }
+
     public function getOrCreateCart(string $userUuid, ?string $cartUuid = null): CartDTO
     {
+        $cartStatusRepo = resolve(CartStatusRepositoryInterface::class);
+
         $baseCartData = [
             'user_id' => $userUuid,
-            'cart_status_id' => CartStatus::query()
-                ->where(['name' => CartStatus::ACTIVE])
-                ->firstOrFail()
-                ->uuid,
+            'cart_status_id' => $cartStatusRepo->getByName(CartStatus::ACTIVE)->uuid,
         ];
 
         return (empty($cartUuid) ?
